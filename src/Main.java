466b5f467c51ba2098c7c7d00b7a2f0a108ec622
Helena Wenzin <helma254@hotmail.com>
2016-09-21 15:51:22 +0200

    adding Stream

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        List<String> items = new ArrayList <String>();
+        items.add("one");
+        items.add("two");
+        items.add("three");
+
+        Stream<String> stream = items.stream();
+
     }
 }
